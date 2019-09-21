219d2badfef0a76a9e510211c4f6eb993c4eef6b
ginpei <ginpei@gmail.com>
2019-09-21 00:18:46 -0700

    add ESLint config

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: [
+    'react-app',
+  ],
+  rules: {
+    // disallow assigning parameters and their properties
+    // 'no-param-reassign': ['error', { 'props': true }],
+  },
+};
